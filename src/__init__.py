afb916eb91d535ab7b5b89ecfc79ff7db9314753
Roman0-5 <chowerroman@gmail.com>
2026-05-18 14:55:50 +0200

    adding module docstrings

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""RA-MAP project: data loading, cleaning, and reporting utilities. 
+! refer to README.md where data is from
+Modules:
+    load_data
+    check_data
+    cleaning
+    clinical_data_audit
+    extraction
+    load_data
+    low_variance_report
+    normalization
+    reporting
+"""
